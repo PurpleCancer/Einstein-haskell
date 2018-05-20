5ec57f50f8e4b609edbdcefb576c5dcbc795316e
Jan Bajer <jan.bajer@gmail.com>
2018-05-20 18:43:56 +0200

    Add some comments for traces of maintainability.

--- EinStein/Logic.hs
+++ EinStein/Logic.hs
@@ -10,6 +10,8 @@ import Data.List
 import System.Random
 import System.Random.Shuffle
 
+-- public
+-- generate a new game state with random setup
 startState :: IO GameState
 startState = do
     g <- newStdGen
@@ -24,27 +26,34 @@ startState = do
     let p1State = [(Player 1, Dice (p1Dice !! 0), Point 4 4),(Player 1, Dice (p1Dice !! 1), Point 4 3),(Player 1, Dice (p1Dice !! 2), Point 4 2),(Player 1, Dice (p1Dice !! 3), Point 3 4),(Player 1, Dice (p1Dice !! 4), Point 3 3),(Player 1, Dice (p1Dice !! 5), Point 2 4)]
     return $ GameState (Player 0) (dice) (p0State ++ p1State)
 
+-- verify whether the point is inside the board
 legalPoint :: Point -> Bool
 legalPoint (Point x y) = (&&) ((&&) (x >= 0) (y >= 0)) ((&&) (x < 5) (y < 5))
 
+-- build a Move object
 moveFromPoint :: Player -> Point -> Point -> Move
 moveFromPoint pl start finish = Move pl start finish
 
+-- generate all legal moves for a player and a point
 genMoves :: Player -> Point -> Moves
 genMoves pl (Point x y) = 
     Moves $ map (moveFromPoint pl (Point x y)) $ filter legalPoint [(Point (x + inc) y), (Point x (y + inc))]
         where inc = if (pl == Player 0) then 1 else -1
 
+-- extract point from the stone tuple
 stone2Point :: (Player, Dice, Point) -> Point
 stone2Point (_, _, p) = p
 
+-- flatten two Moves objects into one
 flattenMoves :: Moves -> Moves -> Moves
 flattenMoves (Moves m1) (Moves m2) = Moves (m1 ++ m2)
 
+-- gets player moves for all the stones supplied in the GameState object
 getPlayerMoves :: GameState -> Moves
 getPlayerMoves (GameState pl _ stones) =
     foldl flattenMoves (Moves []) $ map (genMoves pl) $ map stone2Point $ filter (\(pls, _, _) -> pl == pls) stones
 
+-- filter movable Dice from a list based on the rolled value
 getLegalDice :: Dice -> [Dice] -> [Dice]
 getLegalDice target ds =
     if exact
@@ -64,6 +73,8 @@ getLegalDice target ds =
             filter (\d -> (||) (elem d firstUp) (elem d firstDown)) ds
     where exact = (length $ filter (\d -> d == target) ds) == 1
 
+-- public
+-- gets legal moves from the game state based on the rolled value
 getMoves :: GameState -> Moves
 getMoves (GameState p dice stones) =
     getPlayerMoves (GameState p dice legalStones)
@@ -72,8 +83,14 @@ getMoves (GameState p dice stones) =
           legalDice = getLegalDice dice playerDice
           legalStones = filter (\(_, d, _) -> elem d legalDice) playerStones
 
+-- public
+-- unimplemented
+-- apply a move to the GameState object
 doMove :: GameState -> Move -> GameState
 doMove _ _ = GameState (Player 0) (Dice 0) []
 
+-- public
+-- unimplemented
+-- get a winner if a game state has one
 getWinner :: GameState -> Winner
 getWinner _ = Winner Nothing
