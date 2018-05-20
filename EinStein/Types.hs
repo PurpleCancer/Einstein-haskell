05a601a203eb167d1cbf956cceb6db413f217289
Jan Bajer <jan.bajer@gmail.com>
2018-05-20 15:22:51 +0200

    Add notes and type definitions.

---
+++ EinStein/Types.hs
@@ -0,0 +1,17 @@
+module EinStein.Types
+( Winner(..)
+, Point(..)
+, Move(..)
+, Moves(..)
+, GameState(..)
+) where
+
+data Winner = Maybe Int deriving (Show)
+
+data Point = Point Int Int deriving (Show, Eq)
+
+data Move = Move Int Point Point deriving (Show, Eq)
+
+data Moves = Moves [Move] deriving (Show)
+
+data GameState = GameState Int Int [(Int, Point)]
